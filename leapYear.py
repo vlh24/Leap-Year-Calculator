c8f9515b745fe205d26a5ba774c5258ab67573e5
Valerie Hosler <vlh24@pitt.edu>
2020-02-18 14:21:53 -0500

    Leap Year Calculator

---
+++ leapYear.py
@@ -0,0 +1,23 @@
+checker = False
+while checker == False:
+    try:
+        year = int(input("Input a year: "))
+        if year % 4 == 0:
+            if year % 100 == 0 and year % 400 == 0:
+                print("That is a leap year!" 
+                      + " Enjoy the extra day, " + name + ".")
+                checker = True
+            elif year % 100 == 0 and year % 400 != 0:
+                print("Unfortunately, that isn't a leap year, " 
+                      + name + ".")
+                checker = True
+            else :
+                print("That is a leap year!" 
+                      + " Enjoy the extra day, " + name + ".")    
+                checker = True
+        else:
+            print("Unfortunately, that isn't a leap year, " 
+                      + name + ".")
+            checker = True
+    except ValueError:
+        print("That is not a year! Please input a year, " + name + ".")
